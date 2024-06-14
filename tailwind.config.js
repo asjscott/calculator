1be21d375dc9caddddca37a8c9cfd11af4292002
Alec Scott <asjscott@gmail.com>
2024-06-13 19:44:51 -0700

    added mobile only theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 module.exports = {
   content: ["./src/**/*.{html,js}", "./public/**/*.{html,js}", "./node_modules/tw-elements/js/**/*.js"], 
   theme: {
-    extend: {},
+    extend: {
+      screens: {
+        'mobile-only': { 'max': '991.98px' },
+        },
+    },
   },
   plugins: [require("tw-elements/plugin.cjs")]
 }
