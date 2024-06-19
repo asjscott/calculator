0b23a495bdb4a073be8b3a0adef8b492f605713d
Alec Scott <asjscott@gmail.com>
2024-06-18 21:19:01 -0700

    updated display for 9 digits

--- src/Calculator.js
+++ src/Calculator.js
@@ -17,16 +17,16 @@ const Calculator = () => {
         if (!calculationState.operator) {
             setCalculationState({
                 ...calculationState,
-                operand1: calculationState.operand1 === "0" ? num : calculationState.operand1 === "-0" ? "-" + num : calculationState.operand1 + num,
+                operand1: calculationState.operand1 === "0" ? num : calculationState.operand1 === "-0" ? "-" + num : calculationState.operand1.match(/\d/g).length < 9 ? calculationState.operand1 + num : calculationState.operand1,
             })
-            setDisplayValue(calculationState.operand1 === "0" ? num : calculationState.operand1 === "-0" ? "-" + num : calculationState.operand1 + num)
+            setDisplayValue(calculationState.operand1 === "0" ? num : calculationState.operand1 === "-0" ? "-" + num : calculationState.operand1.match(/\d/g).length < 9 ? calculationState.operand1 + num : calculationState.operand1)
             return;
         }
         setCalculationState({
             ...calculationState,
-            operand2: calculationState.operand2 === "0" ? num : calculationState.operand2 === "-0" ? "-" + num : calculationState.operand2 + num,
+            operand2: calculationState.operand2 === "0" ? num : calculationState.operand2 === "-0" ? "-" + num : calculationState.operand2.match(/\d/g) == null ? num : calculationState.operand2.match(/\d/g).length < 9 ? calculationState.operand2 + num : calculationState.operand2,
         })
-        setDisplayValue(calculationState.operand2 === "0" ? num : calculationState.operand2 === "-0" ? "-" + num : calculationState.operand2 + num)
+        setDisplayValue(calculationState.operand2 === "0" ? num : calculationState.operand2 === "-0" ? "-" + num : calculationState.operand2.match(/\d/g) == null ? num : calculationState.operand2.match(/\d/g).length < 9 ? calculationState.operand2 + num : calculationState.operand2)
     }
 
     function handleOperator(e) {
@@ -51,10 +51,21 @@ const Calculator = () => {
                 operand2: "",
                 operator: operator
             })
-            setDisplayValue(result)
-            return;
+            if ((result >= 1000000000 || result <= -1000000000) || (result < 0.00001 && result > -0.00001)) {
+                setDisplayValue(result.toExponential(5))
+            } else if (String(result).length > 9 && (result < 0.0001 || result > -0.0001)) {
+                setDisplayValue(result.toPrecision(7))
+            } else if (String(result).length > 9 && (result < 0.001 || result > -0.001)) {
+                setDisplayValue(result.toPrecision(8))
+            } else if (String(result).length > 9) {
+                setDisplayValue(result.toPrecision(9))
+            } else {
+                setDisplayValue(result)
+            }  
+            return;    
+            }   
         
-        }
+
         setCalculationState({
             ...calculationState,
             operator: operator
@@ -81,7 +92,17 @@ const Calculator = () => {
             operand2: "",
             equalPressed: true,
         })
-        setDisplayValue(result)
+        if ((result >= 1000000000 || result <= -1000000000) || (result < 0.00001 && result > -0.00001)) {
+            setDisplayValue(result.toExponential(5))
+            } else if (String(result).length > 9 && (result < 0.0001 || result > -0.0001)) {
+            setDisplayValue(result.toPrecision(7))
+            } else if (String(result).length > 9 && (result < 0.001 || result > -0.001)) {
+                setDisplayValue(result.toPrecision(8))
+            } else if (String(result).length > 9) {
+                setDisplayValue(result.toPrecision(9))
+            } else {
+                setDisplayValue(result)
+            } 
     }
 
     function handlePoint() {
@@ -109,8 +130,8 @@ const Calculator = () => {
                 ...calculationState,
                 operand1: calculationState.operand1 === "0" ? "-0" : calculationState.operand1 === "-0" ? "0" : calculationState.operand1 * -1
             })
-            setDisplayValue(calculationState.operand1 === "0" ? "-0" : calculationState.operand1 === "-0" ? "0" : calculationState.operand1 * -1)
-            return
+            setDisplayValue(calculationState.operand1 === "0" ? "-0" : calculationState.operand1 === "-0" ? "0" : displayValue.search(/-/) === 0 ? displayValue.replace("-", "") : "-" + displayValue)
+            return 
         }
         setCalculationState({
             ...calculationState,
@@ -165,7 +186,7 @@ const Calculator = () => {
             <div className='mt-16 mb-3 text-3xl'>Calculator</div>
             <div className="bg-black rounded-3xl p-6 sm:p-10 max-w-[350px] sm:max-w-[450px]">
                 <div>
-                    <div><input type="text" className="bg-black text-white border-0 w-full text-[50px] sm:text-[80px] text-right pr-4 mb-4 rounded-lg" disabled={true} value={displayValue}/></div>
+                    <div><input type="text" className="bg-black text-white border-0 w-full text-[40px] sm:text-[50px] text-right pr-4 mb-4 rounded-lg" disabled={true} value={displayValue}/></div>
                 </div>
                 <div className='grid grid-rows-5'>  
                     <div className='grid grid-cols-4 gap-2 py-1 text-[30px] sm:text-[40px] text-bold text-center'>
@@ -193,7 +214,7 @@ const Calculator = () => {
                         <div><button className="cursor-pointer w-full py-2 sm:py-3.5 sm:px-4 rounded-full text-white bg-amber-500 hover:bg-amber-400" onClick={e => handleOperator(e)}>+</button></div>
                     </div>
                     <div className='grid grid-cols-4  gap-2 py-1 text-[30px] sm:text-[40px] text-bold text-center'>
-                        <div className='px-2 col-span-2 w-full' xs={6}><button className="cursor-pointer w-full py-3.5 px-9 rounded-full text-left btn-zero bg-neutral-800 text-white hover:bg-neutral-600" onClick={(e) => handleNum(e)}>0</button></div>
+                        <div className='col-span-2 w-full'><button className="cursor-pointer w-full py-2 sm:py-3.5 px-9 rounded-full text-left btn-zero bg-neutral-800 text-white hover:bg-neutral-600" onClick={(e) => handleNum(e)}>0</button></div>
                         <div><button className="cursor-pointer w-full py-2 sm:py-3.5 sm:px-4 rounded-full bg-neutral-800 text-white hover:bg-neutral-600" onClick={handlePoint}>.</button></div>
                         <div><button className="cursor-pointer w-full py-2 sm:py-3.5 sm:px-4 rounded-full text-white bg-amber-500 hover:bg-amber-400" onClick={handleEqual}>=</button></div>
                     </div>
